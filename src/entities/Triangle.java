fb5719034ebcbf8edd332b8e088c0c0910257446
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-29 18:52:35 -0300

    Criando metodo para a classe Triangle

--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -2,13 +2,18 @@ package entities;
 
 public class Triangle {
 	
+	//Declarando os parametros da classe Triangle
+	
 	public double a;
 	public double b;
 	public double c;
 	
+	//Criando um método para a classe Triangle
+	
 	public double area() {
 		double p = (a+b+c) / 2.0;
 		double result = Math.sqrt(p * (p-a) * (p-b) * (p-c));
+		return result;
 	}
 	
 
